85937394574d3c8b859185fc74df9ba6f4db8694
thlem <thomas.lemercier.pro@gmail.com>
2015-09-14 13:17:54 +0200

    update image

---
+++ src/app/agilog/project/manage/factory/project.manage.create.factory.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    angular.module('ag.prj.manage').factory('CreateProjectFactory', getCreateProjectFactory);
+
+    var inject = [];
+    getCreateProjectFactory.$inject = inject;
+
+    function getCreateProjectFactory() {
+
+        var ret = [
+
+        ];
+        return ret;
+
+        function submitProjectCreation() {};
+    };
+
+})();
